3ff655db2a2c50bd6c91c5f99ba8956de80381bb
msm <martinconor25@yahoo.com>
2022-10-24 13:48:36 +0300

    working on store

--- lemuroid-app/src/main/java/com/swordfish/lemuroid/app/mobile/feature/store/EpoxyStoreController.kt
+++ lemuroid-app/src/main/java/com/swordfish/lemuroid/app/mobile/feature/store/EpoxyStoreController.kt
@@ -1,6 +1,7 @@
 package com.swordfish.lemuroid.app.mobile.feature.store
 
 import com.airbnb.epoxy.AsyncEpoxyController
+import com.swordfish.lemuroid.R
 import com.swordfish.lemuroid.app.mobile.feature.home.HomeViewModel
 import com.swordfish.lemuroid.app.mobile.feature.home.epoxyHomeSection
 
@@ -13,6 +14,9 @@ class EpoxyStoreController():AsyncEpoxyController() {
         requestModelBuild()
     }
     override fun buildModels() {
-
+        epoxyHomeSection {
+            id("section_discover")
+            title(R.string.favorites)
+        }
     }
 }
